a9f9dd31ffe3f7f92188a8baed45a37f83bc5dc8
krishnamahaliya19 <krishnamahaliya69@gmail.com>
2026-03-18 16:02:47 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,51 @@
+
+# 1. loop control statment
+
+#break 
+# for i in range(1,10):
+#         if i == 5:
+#             break
+#         print(i)
+
+
+# 2. continue
+
+# for i in range(1,6):
+#     if i == 3:
+#         continue 
+#     print(i)
+
+# # 3. print number for 1 to 10 using for loop 
+
+# for i in range(1, 11):
+#     print(i)
+
+# 4. print even numbers from 1 to 20
+
+# for i in range(1, 21):
+#     if i % 2 == 0:
+#         print(i, end='') 
+# for i in range(2, 21, 2):
+#         print(i,end='')
+
+# # 5.print odd number is 1 to 15
+# for i in range(1, 16 ,2):
+#         print(i)
+
+
+# #6.print each character of using string 
+# text = "krishna"
+# for ch in text:
+#     print(ch)
+
+
+# #7.print all item in this list 
+# my_list = [10, 20, 30, 40, 50]
+
+# for i in range(len(my_list)):
+#     print(my_list[i])
+
+
+# # 8. print number is reverse order from 10 to 1
+# for i in range(10, 0, -1):
+#     print(i)
